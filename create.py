2cd5b744c35054f322179a833f07f40e45a51ae0
sage <hello@sageelliott.com>
2023-03-07 22:54:30 -0800

    make generator a function

--- create.py
+++ create.py
@@ -9,41 +9,64 @@ iris = load_iris()
 feature_names = iris.feature_names
 target_names = iris.target_names
 
-# Create empty lists to store the synthetic data
-synthetic_data = []
-synthetic_targets = []
+def create_data(feature_names, target_names, std_mod=0):
+    '''
+    Create synthetic data using the mean and standard deviation of the real data.
+    The synthetic data will be normally distributed with the same mean and standard deviation as the real data.
+    The standard deviation can be modified to increase or decrease the variation in the synthetic data.
 
-# Generate synthetic data for each target class
-for target_idx, target_name in enumerate(target_names):
-    # Get the real data for this target class
-    real_data = iris.data[iris.target == target_idx]
-    # Calculate the mean and standard deviation of each feature for this target class
-    feature_means = np.mean(real_data, axis=0)
-    feature_stds = np.std(real_data, axis=0)
+    Parameters
+    ----------
+    feature_names : list
+        A list of the feature names
+    target_names : list
+        A list of the target names
+    std_mod : float, optional
+        The standard deviation modifier. The default is 0.
 
-    # feature_stds = [x+std_mod for x in feature_stds]
+    Returns
+    -------
+    df : pandas dataframe
+        A dataframe containing the synthetic data and target values
+    '''
 
-    # Generate synthetic data using a normal distribution with the same mean and standard deviation as the real data
-    # divide std for lower variation. Multiply for more?
-    synthetic_data.append(np.random.normal(loc=feature_means, 
-                                           scale=feature_stds*1, 
-                                           size=(50, 4)))
-    
-    # Assign the target value for this synthetic data
-    synthetic_targets.append([target_name]*200)
-    print(f'name: {target_name} - mean: {feature_means} - std: {feature_stds}')
+    # Create empty lists to store the synthetic data
+    synthetic_data = []
+    synthetic_targets = []
 
-# Combine the synthetic data for each target class into a single array and shuffle the rows
-synthetic_data = np.concatenate(synthetic_data, axis=0)
-synthetic_targets = np.concatenate(synthetic_targets, axis=0)
-shuffle_idx = np.random.permutation(len(synthetic_data))
-synthetic_data = synthetic_data[shuffle_idx]
-synthetic_targets = synthetic_targets[shuffle_idx]
+    # Generate synthetic data for each target class
+    for target_idx, target_name in enumerate(target_names):
+        # Get the real data for this target class
+        real_data = iris.data[iris.target == target_idx]
+        # Calculate the mean and standard deviation of each feature for this target class
+        feature_means = np.mean(real_data, axis=0)
+        feature_stds = np.std(real_data, axis=0)
 
-# Create a dataframe with the synthetic data and target values
-df = pd.DataFrame(synthetic_data, columns=feature_names)
-df['target'] = synthetic_targets
+        # feature_stds = [x+std_mod for x in feature_stds]
 
+        # Generate synthetic data using a normal distribution with the same mean and standard deviation as the real data
+        # divide std for lower variation. Multiply for more?
+        synthetic_data.append(np.random.normal(loc=feature_means, 
+                                            scale=feature_stds*std_mod, 
+                                            size=(50, 4)))
+        
+        # Assign the target value for this synthetic data
+        synthetic_targets.append([target_name]*50)
+        print(f'name: {target_name} - mean: {feature_means} - std: {feature_stds}')
 
-# df = df.drop('target', axis=1)
+    # Combine the synthetic data for each target class into a single array and shuffle the rows
+    synthetic_data = np.concatenate(synthetic_data, axis=0)
+    synthetic_targets = np.concatenate(synthetic_targets, axis=0)
+    shuffle_idx = np.random.permutation(len(synthetic_data))
+    synthetic_data = synthetic_data[shuffle_idx]
+    synthetic_targets = synthetic_targets[shuffle_idx]
+
+    # Create a dataframe with the synthetic data and target values
+    df = pd.DataFrame(synthetic_data, columns=feature_names)
+    df['target'] = synthetic_targets
+
+    return df
+
+    # drop target column
+    # df = df.drop('target', axis=1)
 
